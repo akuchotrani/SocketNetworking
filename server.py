7122efd20be3801d182805233d5ef2942baf03ad
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2018-05-21 13:44:56 -0700

    Server receiving image fixed

--- server.py
+++ server.py
@@ -4,12 +4,17 @@ Created on Fri May 18 13:49:43 2018
 
 @author: aakash.chotrani
 """
-
+from PIL import Image
+from PIL import ImageFile
 import socket                   # Import socket module
+import io
+
+ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 port = 60000                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
+print('host name:',host)
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
 
@@ -18,18 +23,22 @@ thanks_message = 'Thank you for connecting'
 while True:
     conn, addr = s.accept()     # Establish connection with client.
     print ('Got connection from', addr)
-    data = conn.recv(1024)
-    print('Server received', repr(data))
+    data = conn.recv(8192)
+#
+#    filename='mytext.txt'
+#    f = open(filename,'rb')
+#    l = f.read(1024)
+#    while (l):
+#       conn.send(l)
+#       print('Sent ',repr(l))
+#       l = f.read(1024)
+#    f.close()
+    print("Server received an Image from client")
+    image = Image.open(io.BytesIO(data))
+    image.show()
+    image.save('server_img.png')
+    
 
-    filename='mytext.txt'
-    f = open(filename,'rb')
-    l = f.read(1024)
-    while (l):
-       conn.send(l)
-       print('Sent ',repr(l))
-       l = f.read(1024)
-    f.close()
-
-    print('Done sending')
+#    print('Done sending')
     conn.sendto(thanks_message.encode(),(host,port))
     conn.close()
