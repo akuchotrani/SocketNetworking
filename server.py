b7cda8fb6a376f755f96341d56a749c317270e55
Aakash Kishore Chotrani <aakashchotrani@gmail.com>
2018-05-23 14:20:36 -0700

    server recvall

--- server.py
+++ server.py
@@ -13,17 +13,32 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 port = 60000                    # Reserve a port for your service.
 s = socket.socket()             # Create a socket object
+print(s)
 host = socket.gethostname()     # Get local machine name
 print('host name:',host)
 s.bind((host, port))            # Bind to the port
 s.listen(5)                     # Now wait for client connection.
+
+
+def recvall(sock):
+    BUFF_SIZE = 4096 # 4 KiB
+    Image_Size = 0
+    data = b''
+    while True:
+        part = sock.recv(BUFF_SIZE)
+        data += part
+        if len(part) < BUFF_SIZE:
+            # either 0 or end of data
+            break
+        Image_Size += 4096
+        print(Image_Size)
+    return data
 
 print ('Server listening....')
 thanks_message = 'Thank you for connecting'
 while True:
     conn, addr = s.accept()     # Establish connection with client.
     print ('Got connection from', addr)
-    data = conn.recv(8192)
 #
 #    filename='mytext.txt'
 #    f = open(filename,'rb')
@@ -33,12 +48,28 @@ while True:
 #       print('Sent ',repr(l))
 #       l = f.read(1024)
 #    f.close()
+    
     print("Server received an Image from client")
-    image = Image.open(io.BytesIO(data))
+    counter = 1
+#    buffer = conn.recv(4096000000)
+    buffer = recvall(conn)
+#    while conn.recv(512) != b'':
+#        print('\n Buffer Received',counter)
+#        if (counter == 1):
+#            buffer = conn.recv(512)
+#        else:
+#            buffer += conn.recv(512)
+#            
+#        if not buffer:
+#            break
+#        counter = counter + 1
+##        print(buffer)
+    image = Image.open(io.BytesIO(buffer))
     image.show()
-    image.save('server_img.png')
+    image.save('server_img.jpg')
+    
     
 
 #    print('Done sending')
-    conn.sendto(thanks_message.encode(),(host,port))
-    conn.close()
+#    conn.sendto(thanks_message.encode(),(host,port))
+#    conn.close()
