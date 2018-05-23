6f155eba05625d970203dedb44e01eda6e5a9e4a
aakashchotrani <aakashchotrani@gmail.com>
2018-05-23 16:11:00 -0700

    clientOpenCv

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ Created on Fri May 18 13:44:22 2018
 """
 
 import socket                   # Import socket module
+import cv2
 
 s = socket.socket()             # Create a socket object
 host = socket.gethostname()     # Get local machine name
@@ -21,21 +22,57 @@ s.connect((SERVER_IP , port))
 #s.sendto(message.encode(),(host,port))
 
 filename = 'meme.jpg'
-image_file = open(filename,'rb')
-image_size = 0;
-while(True):
-    data = image_file.read(4096)
-    if not data:
-        print('data is empty')
-        break
+
+last_image_captured = ""
+def Capture_Webcam_Image():
+    cam = cv2.VideoCapture(0)
+    image_counter = 0
+    while True:
+        ret,frame = cam.read()
+        cv2.imshow("webcam image",frame)
+        if not ret:
+            break
+        
+        key = cv2.waitKey(1)
+        
+        if key%256 == 27:
+            print("Escape pressed, closing....")
+            break
+        elif key%256 == 32:
+            #Space pressed
+            img_name = host + str(image_counter) + ".jpg"
+            cv2.imwrite(img_name, frame)
+            image_counter = image_counter + 1
+
+
+def Send_Image_To_Server():
+    image_file = open(filename,'rb')
+    image_size = 0;
+    while(True):
+        data = image_file.read(4096)
+        if not data:
+            print('data is empty')
+            break
+        
+        image_size += 4096
+        print(image_size)
+        
+        
+        print('sending image....')
+        s.send(data)
+    image_file.close()
+    print('Successfully sent the file')
+    s.close()
+    print('connection closed')
     
-    image_size += 4096
-    print(image_size)
     
+def main():
+    print("Client Code")
+    Capture_Webcam_Image() 
+    Send_Image_To_Server()
+
+
+if __name__ == "__main__":
+    main()
     
-    print('sending image....')
-    s.send(data)
-image_file.close()
-print('Successfully sent the file')
-s.close()
-print('connection closed')
+
